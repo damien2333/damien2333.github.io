a3034cf324ddcf4726ab5850398687c7f1a9a2d8
damien2333 <91968951+damien2333@users.noreply.github.com>
2025-03-13 19:33:53 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function loadPage(page) {
-    fetch(page)
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById("dynamic-content").innerHTML = html;
-        })
-        .catch(error => console.error("Erreur de chargement :", error));
-}
