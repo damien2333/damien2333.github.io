77c69d3520acd0e3fe2166c1dc2115346e89f9e6
damien2333 <91968951+damien2333@users.noreply.github.com>
2025-03-13 18:58:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function loadPage(page) {
-    fetch(page)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('content').innerHTML = data;
-        })
-        .catch(error => console.error('Erreur de chargement :', error));
+    const content = document.getElementById("dynamic-content");
+    content.classList.add("fade-out");
+
+    setTimeout(() => {
+        fetch(page)
+            .then(response => response.text())
+            .then(data => {
+                content.innerHTML = data;
+                content.classList.remove("fade-out");
+                content.classList.add("fade-in");
+            });
+    }, 500);
 }
